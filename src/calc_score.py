ad99dd33ce289d8e4c43450ccdd2397bac229997
sashiming <m.ymd041807@gmail.com>
2024-06-25 13:26:57 +0900

    辞書末尾の空行を削除

---
+++ src/calc_score.py
@@ -0,0 +1,10 @@
+def calc_score(data, dict1, dict2):
+    '''
+    args:
+        data(list): 各文章ごとにMeCabの形態素解析をかけたものが入る想定
+        dict1(dict): dictionary1.txtを前処理したもの
+        dict2(Info2): dictionary2.txtを前処理したもの
+    returns:
+        score: 各文章のスコア
+    '''
+    return 0
