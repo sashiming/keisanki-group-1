25c0e4bd0d9f8bc3d194ee5519d72de5671a42a7
KodamaD <kodkodaman@gmail.com>
2024-06-26 14:49:46 +0900

    出力方法を修正

--- src/main.py
+++ src/main.py
@@ -13,9 +13,17 @@ dict1 = preprocess_dict.preprocess_dict(dict1_str.splitlines())
 dict2 = preprocess_dict2.Info2(dict2_str.splitlines())
 scores = calc_score.calc_score(data, dict1, dict2)
 
+set_sentences = set()
+score_sentences = []
+for (s, p) in zip(sentences, scores):
+    if s not in set_sentences:
+        set_sentences.add(s)
+        score_sentences.append((p, s))
+score_sentences.sort()
+max_len = max(format_string.calc_length(s) for s in sentences)
+
 root_dir = os.path.dirname(os.path.dirname(__file__))
 result_path = os.path.join(root_dir, 'data', 'result.txt')
 with open(result_path, 'w') as file:
-    max_len = max(format_string.calc_length(s) for s in sentences)
-    for (s, p) in zip(sentences, scores):
+    for (p, s) in score_sentences:
         file.write(f"{format_string.align_text(s, max_len)} | {p:10.4f}\n")
