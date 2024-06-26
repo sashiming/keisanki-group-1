6c443c7b9bdc71c03f0ab7a66a35bcaf7ca7f068
sashiming <m.ymd041807@gmail.com>
2024-06-26 14:21:12 +0900

    表記揺れ対策(簡易)

--- src/calc_score.py
+++ src/calc_score.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import regex
+import jaconv
 root_path = os.path.dirname(os.path.dirname(__file__))
 
 def calc_score(data, dict1, dict2):
@@ -12,30 +14,32 @@ def calc_score(data, dict1, dict2):
         scores( list(float) ): 各文章のスコアのリスト
     '''
     scores = []
-
     for sentence in data:
         all_morphemes = 0    # 形態素の総数(記号除く)
         score = 0
-        # words_with_morp = []
         is_root = True     # dict2で根にいるかどうか
-        # tmp_str = ''
         for morph in sentence:
             genkei = morph.get_original_form()
             if '記号' not in morph.get_type():
                 all_morphemes += 1
             if '名詞' in morph.get_type():
+                val = 0
                 if genkei in dict1:
                     val = dict1[genkei].value
-                    score += val 
+                else:   # ひらがな/カタカナ表記揺れへの簡易的対策
+                    if jaconv.hira2kata(genkei) in dict1:
+                        val = dict1[jaconv.hira2kata(genkei)].value
+                    if jaconv.kata2hira(genkei) in dict1:
+                        val = dict1[jaconv.kata2hira(genkei)].value
+                score += val
             if (not is_root) or morph.is_declinable():
                 found, w_score, w_typ = dict2.search(genkei)
                 score += w_score
                 if found:
                     is_root = False
-                    # tmp_str += morph.get_raw_str()
                 else:
                     dict2.reset()
-                    # tmp_str = ''
+                    is_root = True
         scores.append(score / all_morphemes)
 
     return scores
